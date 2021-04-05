714a16dad165dad8aea0b7a1538bb0629b2c64b5
victorG <peter200970@hotmail.com>
2021-04-05 14:06:20 -0500

    Create button css module to style calculator buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as styles from '../styles/button.module.css';
 
 function Button(props) {
   const { name, handleClick } = props;
+  const getButtonClass = string => {
+    const operationRegex = /[X,+,÷,-]/;
+    if (string === '+/-') return styles.number;
+    if (operationRegex.test(string)) return styles.operators;
+    return styles.number;
+  };
 
   return (
-    <button onClick={() => { handleClick(name); }} type="button" className="button">{name}</button>
+    <button onClick={() => { handleClick(name); }} type="button" className={`${getButtonClass(name)} ${styles.button}`}>{name}</button>
   );
 }
 
