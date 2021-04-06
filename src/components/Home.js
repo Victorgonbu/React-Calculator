2444da85142d7b4f722904ca48bbd957c7ad539e
victorG <peter200970@hotmail.com>
2021-04-06 18:21:16 -0500

    Remove commented lines of code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import Navbar from './Navbar';
-// import HeroText from './HeroText';
 import HomeContainer from './HomeContainer';
 
 const Home = () => (
