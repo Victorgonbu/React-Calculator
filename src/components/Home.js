1f81fb688b96dc8591a352276d7992441e5ec371
victorG <peter200970@hotmail.com>
2021-04-07 15:17:03 -0500

    Merge homeContainer component into Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Navbar from './Navbar';
-import HomeContainer from './HomeContainer';
 
-const Home = () => (
-  <div className="home">
-    <Navbar />
-    <HomeContainer />
-  </div>
-);
+import { LoremIpsum } from 'lorem-ipsum';
+import Navbar from './Navbar';
+
+import Header from './Header';
+import Text from './Text';
+
+const Home = () => {
+  const lorem = new LoremIpsum();
+  const paragraph1 = lorem.generateWords(120);
+  const paragraph2 = lorem.generateWords(150);
+  return (
+    <div className="home">
+      <Navbar />
+
+      <Header text="Welcome to our page" />
+      <Text content={paragraph1} />
+      <Text content={paragraph2} />
+
+    </div>
+  );
+};
 
 export default Home;
