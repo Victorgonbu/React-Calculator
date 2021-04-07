c705dfc0ed2bd4fc535b13c13d88fad18f7d8f89
victorG <peter200970@hotmail.com>
2021-04-07 15:25:31 -0500

    Merge calcualtorContainer component to Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import Calculate from '../logic/calculate';
+import * as calcStyles from '../styles/calculator.module.css';
+import * as styles from '../styles/components.module.css';
+import Header from './Header';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+const Calculator = () => {
+  const [total, setTotal] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [next, setNext] = useState(null);
+
+  const handleClick = buttonName => {
+    const newState = Calculate({ total, next, operation }, buttonName);
+    setTotal(newState.total);
+    setOperation(newState.operation);
+    setNext(newState.next);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className={`${styles.container} ${styles.flexRow}`}>
+        <Header text="Let's do some math!" />
+        <div className={calcStyles.calculator}>
+          <Display result={next || total || '0'} />
+          <ButtonPanel handleClick={handleClick} />
+        </div>
+      </div>
+    </>
+
+  );
+};
+
+export default Calculator;
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,14 +11,14 @@ const Home = () => {
   const paragraph1 = lorem.generateWords(120);
   const paragraph2 = lorem.generateWords(150);
   return (
-    <div className="home">
+    <>
       <Navbar />
 
       <Header text="Welcome to our page" />
       <Text content={paragraph1} />
       <Text content={paragraph2} />
 
-    </div>
+    </>
   );
 };
 
