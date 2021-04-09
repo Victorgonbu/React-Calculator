c7b23e342a9c47c0f70536042d6dc0042337e8f2
Shaqri <peter200970@hotmail.com>
2021-04-09 15:19:22 -0500

    Add all test cases for Home component

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -5,6 +5,9 @@ import Home from '../components/Home';
 
 
 jest.mock('../components/Navbar', () => () => <nav data-testid='navbar' />) ;
+jest.mock('../components/Header', () => () => <nav data-testid='header' />) ;
+jest.mock('../components/Text', () => () => <nav data-testid='text' />) ;
+
 
 describe('Home', () => {
 
@@ -13,9 +16,15 @@ describe('Home', () => {
     expect(await screen.getByTestId(/navbar/)).toBeInTheDocument();
   });
   
-  it('should render a welcome message in home header', async () => {
+  it('should render header component with a welcome message', async () => {
     render(<Home />);
 
-    expect( await screen.findByText('Welcome to our page')).toMatchSnapshot();
+    expect( await screen.getByTestId(/header/)).toBeInTheDocument();
+  });
+
+  it('should render two parragraphs using Text component', async () => {
+    render(<Home />);
+
+    expect(await screen.getAllByTestId(/text/)).toHaveLength(2);
   });
 });
