fd4f28a0c2ccda67ce3d305b81848f2131e97b0d
Shaqri <peter200970@hotmail.com>
2021-04-11 11:20:31 -0500

    Add test cases for style in Button.test.js

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import Button from '../components/Button';
+import * as styles from '../styles/components.module.css';
 
 describe('Button', () => {
     const {name, handleClick, color, width} = {
@@ -11,9 +12,20 @@ describe('Button', () => {
         width: 1
     };
   
-    it('renders 1 instance of button Component', async () => {
+    it('renders 1 instance of Button Component as a number', async () => {
         render(<Button key={name} name={name} handleClick={handleClick} color={color} width={width}/>);
-        expect(await screen.getByText('4')).toBeInTheDocument();
+        const button = await screen.getByText('4');
+        expect(button).toBeInTheDocument();
+        expect(button.classList.contains(styles.number)).toBe(true);
+    });
+
+    it('renders 1 instance of Button component as an operator', async () => {
+        const operator = '+';
+        const orange = 'orange';
+        render(<Button key={operator} name={operator} handleClick={handleClick} color={orange} width={width}/>);
+        const operation = await screen.getByText(operator);
+        expect(operation).toBeInTheDocument();
+        expect(operation.classList.contains(styles.operator)).toBe(true);
     });
 
     it('Call parent function to handle button click event', async () => {
