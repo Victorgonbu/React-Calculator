b2212fd4c5229d52b6a854a9900f23d7e2b184e3
Shaqri <peter200970@hotmail.com>
2021-04-11 13:12:08 -0500

    Fix eslint errors

--- src/__tests__/Quote.test.js
+++ src/__tests__/Quote.test.js
@@ -2,23 +2,21 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Quote from '../components/Quote';
-
+/* eslint-disable react/display-name */
 jest.mock('../components/Navbar', () => () => <nav data-testid="navbar" />);
 
 describe('Quote', () => {
-    it('renders Navbar component', async () => {
-        render(<Quote />);
-        const navbar = await screen.getByTestId(/navbar/);
-        expect(navbar).toBeInTheDocument();
-        expect(navbar).toMatchSnapshot();
-    });
+  it('renders Navbar component', async () => {
+    render(<Quote />);
+    const navbar = await screen.getByTestId(/navbar/);
+    expect(navbar).toBeInTheDocument();
+    expect(navbar).toMatchSnapshot();
+  });
 
-    it('renders Text component with a quote', async () => {
-        render(<Quote />);
-        const text = await screen.getByText('Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul');
-        expect(text).toBeInTheDocument();
-        expect(text).toMatchSnapshot();
-    });
-
-    
+  it('renders Text component with a quote', async () => {
+    render(<Quote />);
+    const text = await screen.getByText('Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul');
+    expect(text).toBeInTheDocument();
+    expect(text).toMatchSnapshot();
+  });
 });
