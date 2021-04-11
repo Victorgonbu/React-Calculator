ec0cf6532903bb66bc20f6ce8e73d4607716e93b
Shaqri <peter200970@hotmail.com>
2021-04-11 13:07:48 -0500

    Add test cases for Quote component

--- src/__tests__/Quote.test.js
+++ src/__tests__/Quote.test.js
@@ -3,7 +3,22 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Quote from '../components/Quote';
 
+jest.mock('../components/Navbar', () => () => <nav data-testid="navbar" />);
 
 describe('Quote', () => {
-    it('');
+    it('renders Navbar component', async () => {
+        render(<Quote />);
+        const navbar = await screen.getByTestId(/navbar/);
+        expect(navbar).toBeInTheDocument();
+        expect(navbar).toMatchSnapshot();
+    });
+
+    it('renders Text component with a quote', async () => {
+        render(<Quote />);
+        const text = await screen.getByText('Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul');
+        expect(text).toBeInTheDocument();
+        expect(text).toMatchSnapshot();
+    });
+
+    
 });
